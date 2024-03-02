ec31c62caf45a18b8df1478c9b85f9e145a40699
Jonathan <54237352+not-enough-ram@users.noreply.github.com>
2024-03-02 20:52:12 +0100

    disabled 2nd controller for mygames

--- backend/src/Controller/GameController.ts
+++ backend/src/Controller/GameController.ts
@@ -1,30 +1,30 @@
-import {
-  Controller,
-  Post,
-  Body,
-  UploadedFile,
-  UseInterceptors,
-} from "@nestjs/common";
-import { FileInterceptor } from "@nestjs/platform-express";
-import { CreateGameDto } from "../Interfaces/Types";
-import { GameSessionService } from "../Service/GameSessionService";
-import { uploadImage } from "../Service/FileService";
+// import {
+//   Controller,
+//   Post,
+//   Body,
+//   UploadedFile,
+//   UseInterceptors,
+// } from "@nestjs/common";
+// import { FileInterceptor } from "@nestjs/platform-express";
+// import { CreateGameDto } from "../Interfaces/Types";
+// import { GameSessionService } from "../Service/GameSessionService";
+// import { uploadImage } from "../Service/FileService";
 
-@Controller("my-games")
-export class GamesController {
-  constructor(private gameSessionService: GameSessionService) {}
+// @Controller("my-games")
+// export class GamesController {
+//   constructor(private gameSessionService: GameSessionService) {}
 
-  @Post()
-  @UseInterceptors(FileInterceptor("image")) // 'image' is the name of the file field in your form
-  async createGame(
-    @UploadedFile() file: Express.Multer.File,
-    @Body() createGameDto: CreateGameDto,
-  ) {
-    const imageUrl: string = uploadImage(createGameDto.image);
-    const gameSession = this.gameSessionService.createGameSession(
-      createGameDto,
-      imageUrl,
-    );
-    return gameSession;
-  }
-}
+//   @Post()
+//   @UseInterceptors(FileInterceptor("image")) // 'image' is the name of the file field in your form
+//   async createGame(
+//     @UploadedFile() file: Express.Multer.File,
+//     @Body() createGameDto: CreateGameDto,
+//   ) {
+//     const imageUrl: string = uploadImage(createGameDto.image);
+//     const gameSession = this.gameSessionService.createGameSession(
+//       createGameDto,
+//       imageUrl,
+//     );
+//     return gameSession;
+//   }
+// }
